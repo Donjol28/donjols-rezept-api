c99ba27c3a9da968d7d57e3ce34d89a259224d5d
Daniel Albrecht <dani131albrecht@gmail.com>
2025-02-07 14:20:19 +0100

    Update Text

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ def home():
 @app.get("/rezepte")
 def get_rezepte():
     return rezepte
+
+
+#---------------------------------------------------------------------------------
+# after changing the file, please use cmd and run these commands to update the API 
+
+# git add main.py
+# git commit -m "Port auf 8080 geändert"
+# git push
