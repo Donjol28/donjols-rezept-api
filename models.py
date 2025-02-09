1680480d38df6cca15f9bd1cc7d57d723b8d21f7
Daniel Albrecht <dani131albrecht@gmail.com>
2025-02-09 19:32:39 +0100

    models update

--- models.py
+++ models.py
@@ -3,9 +3,9 @@ from tortoise import fields
 
 class Rezept(Model):
     id = fields.IntField(pk=True)
+    
     name = fields.CharField(max_length=255)
-    beschreibung = fields.CharField(null=True)
-    category = fields.CharField(null=True)
-
+    beschreibung = fields.TextField(null=True)
+    category = fields.TextField(null=True)
     def __str__(self):
         return self.name
